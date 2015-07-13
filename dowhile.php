106f19972dfcda635e7bffc72064dc7831964ff2
Kinga Gierszewska <czarneokoproroka@gmail.com>
2015-07-13 13:25:31 +0200

    SUdo dowhile

---
+++ dowhile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Your own do-while</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php
+        //write your do-while loop below
+        $i = 5;
+        do {
+            echo "Powtarzaj ten tekst 5 razy \n";
+            $i = $i - 1;
+            }
+            while ($i > 0);
+    ?>
+    </body>
+</html>
